4c1973d4f03ac19a03b0af63381a5167f84d2a47
japanoise <japanoise@tfwno.gf>
2016-09-14 23:47:26 +0100

    play nice with chocodoom

--- dwm-6.1.h
+++ dwm-6.1.h
@@ -27,6 +27,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "chocolate-doom",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Pale Moon",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
